a0455261dcdd18b6b2443e9f83e49afa5608c5db
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2023-09-06 17:42:36 +0200

    First commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.ranking': 'Ranking',
+        'nav.qualifying': 'Qualifying',
+    },
+    es: {
+        'nav.home': 'Inicio',
+        'nav.ranking': 'Clasificación',
+        'nav.qualifying': 'Quali',
+    },
+} as const;
