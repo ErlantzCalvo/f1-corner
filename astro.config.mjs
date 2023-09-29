c96ddfed384e5f3a25f2d6b1f3b88d28ec138f04
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2023-09-29 17:34:04 +0200

    Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import prefetch from "@astrojs/prefetch";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [prefetch()],
   image: {
     domains: ["upload.wikimedia.org"],
