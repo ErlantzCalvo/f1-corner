20158751c0b9cfa8ea4e2aa2ed2a7094a36074d8
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2023-10-04 17:45:06 +0200

    Sync changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
-import vercelStatic from '@astrojs/vercel/static';
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: "static",
   adapter: vercelStatic({
     webAnalytics: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
   integrations: [prefetch()],
   image: {
