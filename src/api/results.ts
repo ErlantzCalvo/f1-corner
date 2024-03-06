a3fdfbbe14fb753b153e93cce676a3d638485ab2
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2024-03-06 11:03:25 +0100

    Fix

--- src/api/results.ts
+++ src/api/results.ts
@@ -1,7 +1,7 @@
 import type { Race, RaceInfo } from "../types";
 
 export const getRaceResults = function (race: string): Promise<Race> {
-  return fetch(`http://ergast.com/api/f1/current/${race}/results.json`)
+  return fetch(`https://ergast.com/api/f1/current/${race}/results.json`)
     .then((res) => res.json())
     .then((data) => data as Promise<RaceInfo>)
     .then((data) => data.MRData.RaceTable!.Races[0]);
--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -1,13 +1,5 @@
 import type { MRData, RaceInfo, RaceTable } from "../types";
 
-const toJson = (res: Response) => {
-  if (res.status === 404) {
-    return undefined;
-  }
-
-  return res.json();
-};
-
 export const getNextRaceData = (): Promise<RaceTable> => {
   return fetch("https://ergast.com/api/f1/current/next.json")
     .then((res) => res.json())
