1ce6a143c534f2152a90882a44d5da7ce1feb88c
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2024-03-27 15:20:25 +0100

    Performance improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercelStatic from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://f1-corner-iota.vercel.app/",
   adapter: vercelStatic({
     webAnalytics: {
       enabled: true,
